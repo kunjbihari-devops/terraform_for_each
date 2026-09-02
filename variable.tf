391c5e1bf21132403175a47290bdde7b48a5aa11
kunjbihari-devops <kunjbihari1989singh@gmail.com>
2026-09-03 00:42:12 +0530

    Add Terraform for_each configuration

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  description = "Azure Resource Group ka naam"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure Resource Group ki location"
+  type        = string
+}
